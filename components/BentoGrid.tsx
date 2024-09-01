768316d9b55fc009e38d1fcecc8aa05b8fb9534d
wodenp98 <paulvigneron4698@gmail.com>
2024-09-01 22:29:23 +0200

    Add with and height for screen

--- components/BentoGrid.tsx
+++ components/BentoGrid.tsx
@@ -2,7 +2,11 @@
 import React from "react";
 
 const BentoGrid = ({ children }: any) => {
-  return <div className="grid grid-cols-12 grid-rows-12 gap-4">{children}</div>;
+  return (
+    <div className="grid grid-cols-12 grid-rows-12 gap-4 w-screen h-screen">
+      {children}
+    </div>
+  );
 };
 
 export default BentoGrid;
